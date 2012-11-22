ba3b41887c1f762fa4ffe376e1c244779455215a
jahayes91 <jahayes1991@gmail.com>
2012-11-22 19:48:43 +0000

    Flashing Lights

--- pi-light.py
+++ pi-light.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as gpio
+import time
 
 #Set mode
 gpio.setmode(gpio.BCM)
@@ -20,7 +21,18 @@ gpio.output(23, gpio.LOW)
 
 #Loop LED's from 14 to 23
 while True:
+	time.sleep(0.1)
 	gpio.output(14, gpio.HIGH)
+	time.sleep(0.1)
 	gpio.output(15, gpio.HIGH)
+	time.sleep(0.1)
 	gpio.output(18, gpio.HIGH)
+	time.sleep(0.1)
 	gpio.output(23, gpio.HIGH)
+	time.sleep(0.1)
+
+	gpio.output(14, gpio.LOW)
+	gpio.output(15, gpio.LOW)
+	gpio.output(18, gpio.LOW)
+	gpio.output(23, gpio.LOW)
+
