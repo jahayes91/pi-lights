9a88c190bf7c32e8322880dd0e3c0fe83c0641ca
jahayes91 <jahayes1991@gmail.com>
2012-11-25 18:03:04 +0000

    Follow Program

--- xmas.py
+++ xmas.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python -tt
+
 # import required modules
 import RPi.GPIO as gpio
 import time
@@ -24,41 +26,54 @@ for pins in pin_out:
 for pins in pin_out:
   gpio.output(pins, gpio.LOW)
 
-# Test Program 1
+# Keep Program looping
 while True:
-  input_value = gpio.input(17)
 
+  # Check button status
+  input_value = gpio.input(17)
+  
+  # Increment Button Count
   if input_value == False:
     but_count = but_count + 1
 
+    # Loop button count
     if but_count > 1:
       but_count = 0
 
+    
     while input_value == False:
       input_value = gpio.input(17)
 
+  # If button count if 0 then run first light program
   if but_count == 0:
 
+    # Loop through pins and switch each one on
     for pins in pin_out:
       time.sleep(0.1)
       gpio.output(pins, gpio.HIGH)
- 
+      input_value == gpio.input(pin_in[0])
     time.sleep(0.1)
-
+    
+    # Switch off all pins to begin another iteration
     for pins in pin_out:
       gpio.output(pins, gpio.LOW)
+      input_value == gpio.input(pin_in[0])
 
-      if input_value == False:
-        break
-
+  # If count = 1 run second light program
   if but_count == 1:
 
+    # Turn all pins on
     for pins in pin_out:
       gpio.output(pins, gpio.HIGH)
+      input_value == gpio.input(pin_in[0])
+    
+    # Wait .7 of a second
+    time.sleep(0.4)
 
-    time.sleep(0.7)
-
+    # Turn all pins off
     for pins in pin_out:
       gpio.output(pins, gpio.LOW)
-
-    time.sleep(0.7)
+      input_value == gpio.input(pin_in[0])
+    
+    # Wait .7 of a second
+    time.sleep(0.4)
